3a67726fc1aaff295da4607fb438d009c94421e4
Marco Antonio Caldeira <caldeiran391@gmail.com>
2025-06-29 19:43:04 -0300

    Adicionar elementos a lista

---
+++ src/com/marco/estruturadedados/vetor/Vetor.java
@@ -0,0 +1,33 @@
+package com.marco.estruturadedados.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+    private int tamanho;
+
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+//    public void adiciona(String elemento) {
+//        for(int i=0; i<elementos.length; i++) {
+//            if(elementos[i] == null) {
+//                elementos[i] = elemento;
+//                break;
+//            }
+//        }
+//    }
+
+
+    public void adiciona(String elemento) throws Exception{
+        if(this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("Vetor já está cheio");
+        }
+
+    }
+}
---
+++ src/com/marco/estruturadedados/vetor/teste/Aula2.java
@@ -0,0 +1,12 @@
+package com.marco.estruturadedados.vetor.teste;
+import com.marco.estruturadedados.vetor.Vetor;
+
+public class Aula2 {
+
+    public static void main(String[] args) {
+       Vetor vetor = new Vetor(5);
+
+       var i = 0;
+       i++;
+    }
+}
