44f90c320335e37431959da9d7a315fb979ccc37
Marco Antonio Caldeira <caldeiran391@gmail.com>
2025-07-08 08:17:12 -0300

    Aulas de Pilha

---
+++ src/com/marco/estruturadedados/pilha/teste/Aula15.java
@@ -0,0 +1,16 @@
+package com.marco.estruturadedados.pilha.teste;
+
+public class Aula15 {
+
+    public static void main(String[] args) {
+
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        System.out.println(pilha.estaVazia());
+
+        pilha.empilha(1);
+
+        System.out.println(pilha.estaVazia());
+
+    }
+}
