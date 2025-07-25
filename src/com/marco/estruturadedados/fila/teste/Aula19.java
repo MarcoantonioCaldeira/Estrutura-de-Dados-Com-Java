344cae16f1702daa4f449889823554e267a84fef
Marco Antonio Caldeira <caldeiran391@gmail.com>
2025-07-25 18:21:48 -0300

    Aula 19, 20 e 21

---
+++ src/com/marco/estruturadedados/fila/teste/Aula19.java
@@ -0,0 +1,12 @@
+package com.marco.estruturadedados.fila.teste;
+
+public class Aula19 {
+    public static void main(String[] args) {
+
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.estaVazia()); //true
+        System.out.println(fila.tamanho()); // 0
+
+    }
+}
